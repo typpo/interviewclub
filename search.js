bbf651c44a303df304a438d8f53e5ec667c93a1f
Julius Elinson <jelinson@hmc.edu>
2015-02-27 18:18:47 -0800

    remove search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-$('#company-signup').on('submit', function(e) {
-
-    // Prevent Default Submit Event
-    e.preventDefault();
-
-    // Get data from the form and put them into variables
-    var data = $(this).serializeArray();
-    console.log(data);
-});
