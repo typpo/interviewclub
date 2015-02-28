ad8fb1fa182671901861c51895ddbeb2ad39ec1e
Ian Webster <typppo+github@gmail.com>
2015-02-27 20:25:06 -0800

    mailing

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,9 +1,41 @@
+var secrets = require('../secrets.js');
+var sparkpost = require('sparkpost')({key: secrets.key});
+
 exports.home = function(req, res) {
   handle(req, res);
 }
 
 exports.main = function(req, res) {
   handle(req, res);
+};
+
+exports.request_interview = function(req, res) {
+  var trans = {};
+
+  // Set some metadata for your email
+  trans.campaign = 'interview-mail';
+  trans.from = 'manager@gointerview.club';
+  trans.subject = 'Someone wants to pay you to interview';
+
+  // Add some content to your email
+  //trans.html = '<html><body><h1>Congratulations, {{name}}!</h1><p>You just sent your very first mailing!</p></body></html>';
+  trans.text = '{{company}} would like to pay you $xx for an hour of your time.  Accept?';
+  trans.substitutionData = {company: req.company};
+
+  // Pick someone to receive your email
+  trans.recipients = [{ address: { name: 'gointerview.club', email: req.email } }];
+
+  // Send it off into the world!
+  sparkpost.transmission.send(trans, function(err, res) {
+    if (err) {
+      console.log('Whoops! Something went wrong');
+      console.log(err);
+    } else {
+      console.log('Woohoo! You just sent your first mailing!');
+    }
+  });
+
+  res.send('ok');
 };
 
 function handle(req, res) {
---
+++ app/secrets_example.js
@@ -0,0 +1,3 @@
+exports = module.exports = {
+  key: 'be15089f45ac01d3f9e086f0498218a57f2861d9'
+};
