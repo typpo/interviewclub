d6ff6a723c8b83cc1672df325fd3d98d77c1fd8b
Andy Kearney <akearney@hmc.edu>
2015-02-27 19:36:00 -0800

    Load in skills, widget to add and remove them from lists

--- edit.js
+++ edit.js
@@ -4,6 +4,8 @@ Parse.initialize("WYKBPP1wtAdbqiTfjKvkrWhEObFvll67wivhst20", "O1AvRyOcTE1aUV9Lvd
 
 var Expertise = Parse.Object.extend('Expertise');
 var expertiseQuery = new Parse.Query(Expertise);
+var unselected_roles = [];
+var selected_roles = [];
 
 var currentUser = Parse.User.current();
 if (!currentUser) {
@@ -15,18 +17,28 @@ if (currentUser.price) {
   // TODO update price string
 }
 
-var currentRoles = currentUser.get('roles') || [];
+var currentRoles = currentUser.get('expertise') || [];
 
 expertiseQuery.find().then(function(results) {
   console.log(results);
-  var html = getRolesPills(results);
-  $('.role_pills').html(html);
+  unselected_roles = results.filter(function(role) {
+    for (var i in currentRoles) {
+      if (currentRoles[i].id == role.id) {
+        return false;
+      }
+    }
+    return true;
+  });
+  var html = getRolesPills(unselected_roles);
+  $('.unselected_role_pills').html(getRolesPills(unselected_roles));
+  $('.selected_role_pills').html(getRolesPills(selected_roles));
 }, function (error) {
   console.log(error);
 });
 
 $('.form-editExpert').on('submit', function(e) {
   console.log('save the info to the user and the user to the roles');
+
 });
 
 var getRolesPills = function(roles) {
@@ -34,10 +46,46 @@ var getRolesPills = function(roles) {
   for (var i in roles) {
     pills_html.push(tmpl('role_pill', {
       role: {
+        id: roles[i].id,
         name: roles[i].get('name')
       }
     }));
   }
   console.log(pills_html);
   return pills_html.join('');
-}
+};
+
+var unselectRole = function(e) {
+  var id = $(this).data('id');
+  for (var i in selected_roles) {
+    if (selected_roles[i].id == id) {
+      var role = selected_roles.splice(i, 1)[0];
+      addRole(unselected_roles, role);
+    }
+  }
+  $('.unselected_role_pills').html(getRolesPills(unselected_roles));
+  $('.selected_role_pills').html(getRolesPills(selected_roles));
+};
+
+var selectRole = function(e) {
+  var id = $(this).data('id');
+  for (var i in unselected_roles) {
+    if (unselected_roles[i].id == id) {
+      var role = unselected_roles.splice(i, 1)[0];
+      addRole(selected_roles, role);
+    }
+  }
+  $('.unselected_role_pills').html(getRolesPills(unselected_roles));
+  $('.selected_role_pills').html(getRolesPills(selected_roles));
+};
+
+var addRole = function(role_list, role) {
+  role_list.push(role);
+  role_list.sort(function(a, b) {
+    return a.get('name') > b.get('name');
+  });
+};
+
+$('.selected_role_pills').on('click', '.role', unselectRole);
+$('.unselected_role_pills').on('click', '.role', selectRole);
+
