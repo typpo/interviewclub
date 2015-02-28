f2b046e5508c698fc882e75ea367cafbafeea5a0
Ian Webster <typppo@gmail.com>
2015-02-28 01:14:36 -0800

    port 31313

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 31313);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'hjs');
   //app.use(express.favicon());
