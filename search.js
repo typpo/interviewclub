ebfd9b309ccb601245cf8915e70313882862c516
Julius Elinson <jelinson@hmc.edu>
2015-02-27 18:18:00 -0800

    bootstrap modal

--- search.js
+++ search.js
@@ -1,9 +1,9 @@
-$('#expertise-selection').on('submit', function(e) {
+$('#company-signup').on('submit', function(e) {
 
     // Prevent Default Submit Event
     e.preventDefault();
 
     // Get data from the form and put them into variables
     var data = $(this).serializeArray();
-
+    console.log(data);
 });
