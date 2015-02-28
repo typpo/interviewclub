b6715721ebe60ad77c6b0113573bb181ff9f56e2
Ian Webster <typppo+github@gmail.com>
2015-02-27 18:54:08 -0800

    filtering

--- list.js
+++ list.js
@@ -1,20 +1,49 @@
 'use strict';
 
-$(function() {
-  exampleBox();
-  exampleBox();
-  exampleBox();
-  exampleBox();
-});
-
-function exampleBox() {
-  addBox({
+var data = [
+{
     name: 'Andy Kearney',
     tags: 'javascript, java, jslayout, angular, chrome extensions',
     desc: 'I love pizza and have screened thousands of resumes and conduct interviews for Google.  I enjoy focusing on coding and algo questions that mimic situations your candidates will encounter in the real world.',
     hourly: 150,
     img: 'http://i.imgur.com/7NIuzYp.png',
+}
+];
+
+$(function() {
+  // TODO get data
+  data.forEach(function(obj) {
+    addBox(obj);
   });
+
+  var t = null;
+  $('#search').on('keydown', function() {
+    if (t) clearTimeout(t);
+    t = setTimeout(function() {
+      var val = $('#search').val();
+      if (val.length > 2) {
+        $('#searchterm-container').show();
+        filter(val);
+      } else {
+        $('#searchterm-container').hide();
+      }
+    }, 250);
+  });
+});
+
+function filter(term) {
+  $('#boxes').html('');
+  data.forEach(function(obj) {
+    var str = obj.name + obj.tags + obj.desc;
+    if (str.toLowerCase().indexOf(term) > -1) {
+      addBox(obj);
+    }
+  });
+  $('#searchterm').text(term);
+}
+
+function exampleBox() {
+  addBox();
 }
 
 function addBox(opts) {
