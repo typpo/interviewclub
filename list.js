282e3923d662f3563be709eaad298d201a0d4251
Ian Webster <typppo+github@gmail.com>
2015-02-27 17:18:23 -0800

    dynamic list

--- list.js
+++ list.js
@@ -1,1 +1,19 @@
-console.log('hello list');
+'use strict';
+
+$(function() {
+
+  addBox({
+    name: 'Andy Kearney',
+    tags: 'javascript, java, jslayout, angular, chrome extensions',
+    desc: 'I love pizza and have screened thousands of resumes and conduct interviews for Google.  I enjoy focusing on coding and algo questions that mimic situations your candidates will encounter in the real world.',
+    hourly: 150,
+    img: 'http://i.imgur.com/7NIuzYp.png',
+  });
+
+  console.log('it is done');
+});
+
+function addBox(opts) {
+  var $box = tmpl(document.getElementById('box-template').innerHTML, opts);
+  $('#boxes').append($box);
+}
