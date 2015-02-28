e41cd4d9ddd6f94bef4bb2a5da446adfa1f68347
Julius Elinson <jelinson@hmc.edu>
2015-02-28 14:38:52 -0800

    no results message

--- web/list.js
+++ web/list.js
@@ -89,6 +89,12 @@ function applyFilters() {
 }
 
 function updateVisibleBoxs(usersToShow) {
+  if (!usersToShow.length) {
+    $('#zerohero').show();
+  } else {
+    $('#zerohero').hide();
+  }
+
   for (var i in userToBox) {
     if ($.inArray(i, usersToShow) !== -1) {
       userToBox[i].show();
