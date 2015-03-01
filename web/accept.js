53b509d3348377de6a5031365a5c1e83154a91da
Julius Elinson <jelinson@hmc.edu>
2015-02-28 18:05:53 -0800

    add state to accept link

--- web/accept.js
+++ web/accept.js
@@ -2,19 +2,21 @@ Parse.$ = jQuery;
 Parse.initialize("WYKBPP1wtAdbqiTfjKvkrWhEObFvll67wivhst20", "O1AvRyOcTE1aUV9LvdiJ95Acg9EGyWIgpNf9WNCy");
 
 var InterviewRequest = Parse.Object.extend('InterviewRequest');
+
+var ACCEPTED = "ACCEPTED";
 $(function() {
   var requestId = getParameterByName('id');
   console.log(requestId);
   new Parse.Query(InterviewRequest).get(requestId, {
     success: function(ir) {
-      ir.set('state', 'ACCEPTED');
+      ir.set('state', ACCEPTED);
       ir.save();
 
       // send email to company
       // TODO fix this
       $.get('/accept?email=' + ir.get('company').get('username') +
             '&company=' + ir.get('company').get('companyName') +
-            '&price=' + ir.get('expert').get('price'), function() {
+            '&price=' + ir.get('expert').get('price') + '&state=' + ACCEPTED, function() {
         setTimeout(function() {
           window.location.href='/edit_profile.html';
         }, 1000);
